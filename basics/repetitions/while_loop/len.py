03ffc0589bd30885d18262dcbb0ff9ba061779a9
Louise Jennings <5jennl29@solent.ac.uk>
2021-02-17 09:50:05 +0000

    Added code to demonstrate the use of a while loop with len function.

---
+++ basics/repetitions/while_loop/len.py
@@ -0,0 +1,5 @@
+print() # line space
+print("Please enter a phrase.")  #Request for user input
+user_phrase = input()  #This is the phrase that will determine the number of repetitions
+
+print(len(user_phrase) * "Bop ") #uses the length (len) of the phrase to determine no. repetitions of the word 'Bop'
