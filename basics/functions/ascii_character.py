aea6c21d42ce5bb4322bf0cc2e150804f2864f8f
Louise Jennings <5jennl29@solent.ac.uk>
2021-02-19 10:47:09 +0000

    Added code to demonstrate the use of built-in functions: len, ord.

---
+++ basics/functions/ascii_character.py
@@ -0,0 +1,11 @@
+print("Program started!")
+print("Please enter an ASCII code: ", end="")
+code = abs(int(input()))
+
+if code in range(32,127):
+    character = chr(code)
+    print(f"The character represented by the ASCII code {code} is {character}")
+else:
+    print("That number is not within range :(")
+
+print("\nProgram ended!")
