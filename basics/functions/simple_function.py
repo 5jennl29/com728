9bd8b671e116fbdc6676bda4abcf73527b8e8645
Louise Jennings <5jennl29@solent.ac.uk>
2021-02-19 10:57:45 +0000

    Added code to demonstrate the use of a user-defined function.

---
+++ basics/functions/simple_function.py
@@ -0,0 +1,8 @@
+# The function
+def listen():
+    print("Please enter a word representing a sound: ",end="")
+    sound = input()
+    print(f"That was a loud {sound}!")
+
+# Call function 'listen'
+listen()
