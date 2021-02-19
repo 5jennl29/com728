db2759546531445ba519d4eb6417533595cd2f5a
Louise Jennings <5jennl29@solent.ac.uk>
2021-02-19 14:49:30 +0000

    Added code to demonstrate the use of a user-defined function with multiple parameters.

---
+++ basics/functions/function_with_parameters.py
@@ -0,0 +1,12 @@
+# Define function 'climb_ladder' with two parameters (in brackets)
+def climb_ladder(steps_remaining, steps_crossed):
+
+    # Function decisions
+    if steps_remaining > steps_crossed:
+        print("Still some way to go!")
+    elif steps_crossed > steps_remaining:
+        print("We are almost there!")
+
+# Function calls with parameters
+climb_ladder(5,2)
+climb_ladder(2,5)
