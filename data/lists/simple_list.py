cd08db0618067062846ed9bd2c552b1d48d59b86
Louise Jennings <5jennl29@solent.ac.uk>
2021-03-05 10:27:26 +0000

    Added comments to simple_list.py file

--- data/lists/simple_list.py
+++ data/lists/simple_list.py
@@ -1,11 +1,11 @@
-def directions():
-    direction_list = ["Move forward", "Move backward", "Turn left", "Turn right"]
-    return direction_list
+def directions():   # Defines 'directions' function
+    direction_list = ["Move forward", "Move backward", "Turn left", "Turn right"]   # Added a list of strings
+    return direction_list    # Returns the list
 
 def run():
-    directions()
-    print(directions())
+    directions()    # Calls function 'directions'
+    print(directions())   # Prints the result of the function 'directions'
 
-run()
+run()            # Calls the run function
 
 
