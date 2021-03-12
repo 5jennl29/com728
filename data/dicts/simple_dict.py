279f8f9bda15ba0fcba42d457a10b1806d68dd26
Louise Jennings <5jennl29@solent.ac.uk>
2021-03-12 14:19:46 +0000

    Added comments to simple_dict file

--- data/dicts/simple_dict.py
+++ data/dicts/simple_dict.py
@@ -1,12 +1,12 @@
 
 def pattern():
-    sequences = {"Short Sequence":3, "Medium Sequence":2, "Long Sequence":1}
-    return sequences
+    sequences = {"Short Sequence":3, "Medium Sequence":2, "Long Sequence":1}          # This is the content of the dictionary
+    return sequences                                                                  # Returns the dictionary
 
 
 def run():
-    pattern_dict = pattern()
-    print(pattern_dict)
+    pattern_dict = pattern()                # Calls the function 'pattern' and assigns the returned dictionary to 'pattern_dict'
+    print(pattern_dict)                     # Prints the dictionary content
 
 
 # call the function when the module is executed directly
