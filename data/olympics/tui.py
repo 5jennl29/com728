618184f24ff663a11226ec62e10b9330741008b4
Louise Jennings <5jennl29@solent.ac.uk>
2021-03-17 09:31:16 +0000

    Added basis of code to implement a text user interface.

---
+++ data/olympics/tui.py
@@ -0,0 +1,89 @@
+import csv    # Imports the csv reader
+
+def load_data(file_path):                   # Function definition with parameter 'file_path'
+    print("Loading data... ", end="")       # Print message
+
+    with open(file_path) as file:           # Opens the file
+        csv_reader = csv.reader(file)       # Reads the file
+        headings = next(csv_reader)         # Passes headings to variable 'headings'
+        for line in csv_reader:                # For the remaining lines in the file
+            records.append(line)             # Add each line to the variable 'records'
+            num_records = len(records)
+
+        print(f"Done!\nSuccessfully loaded {num_records} records.")                      # Print message
+        return records, headings
+
+records = []  # Global variable 'records' = an empty list
+headings = [] # Global variable 'headings' = an empty list
+msg = "Reading data from athlete_events.csv..."
+
+
+def started(msg=""):                        # Function definition with parameter
+    print(f"--------------------------------------------------------------------------------\nOperation started: {msg}...\n")
+
+
+def completed():
+    print("\nOperation completed.\n--------------------------------------------------------------------------------")
+
+
+def error(msg):
+    print(f"Error! {msg}!")
+
+
+def menu():
+    print("""    
+    [years]    List unique years
+    [tally]    Tally up medals
+    [ctally]   Tally up medals for each team
+    [exit]     Exit the program)\n
+""")
+    print("Your selection:",str(input()))
+
+
+def display_medal_tally(tally):
+    gold_medals = silver_medals = bronze_medals = 0
+
+    if data[14] == "gold":
+        gold_medals += 1
+    elif data[14] == "silver":
+        silver_medals += 1
+    elif data[14] == "bronze":
+        bronze_medals += 1
+
+    tally = {f"Gold", {gold_medals}, "Silver", {silver_medals}, "Bronze", {bronze_medals}}
+
+    for key, value in tally.items():                      # For each key-value pair (item) in 'tally' dictionary
+        print(f"|  {key}: {value}  |")                    # Print the key and the corresponding value
+
+    return gold_medals, silver_medals, bronze_medals
+
+
+def display_medal_tally(team_tally):
+
+    print("Analysing data...")
+
+    # Creates dictionary 'team_tally' which calls and stores the results of the other functions as a 'value' with it's 'key' description.
+    team_tally = {f"{data[6]}": display_medal_tally()}
+
+    for key, value in team_tally.items():        # For each item (key-value pair) in 'team_tally'
+        print(f"{key}: {value}")                # Print the key and it's corresponding value
+
+
+def display_years(years):
+    years = set()
+    for value in data[9]:
+        years = (value)                      # Display each value (year)
+        years.add(data)                      # Add that data to the set 'years'
+
+
+    for data in sorted(years, reverse=True):
+        print(f"{years}\n")
+
+
+def run():
+    started(msg)
+    completed()
+    error(msg)
+
+
+run()
