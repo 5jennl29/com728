e2f0ae858df6b3698be3e5fc9567060972c58dd0
Louise Jennings <5jennl29@solent.ac.uk>
2021-03-21 11:26:22 +0000

    Added code to retrieve many records.

---
+++ persistence/select_many.py
@@ -0,0 +1,29 @@
+import sqlite3                                         # Imports SQLite
+
+
+def retrieve_bot():
+    db = sqlite3.connect("bots.db")                    # 'db' is a variable. It's assigned a reference to the database when it connects.
+    cursor = db.cursor()                               # 'cursor' is created
+
+    sql = "SELECT * FROM bots;"                        # This takes the SQLite query to be executed as a parameter (string) value.
+    cursor.execute(sql)                                # 'cursor' object uses the method 'execute' to query the database
+
+    all_rows = cursor.fetchall()                        # Method 'fetchall' gets all records from the db
+
+    db.close()                                          # Closes the database (IMPORTANT!)
+
+    print("First bot in the system:")
+    print(f"{all_rows[0]}\n")
+    print("All bots in the system:")
+    print(f"{all_rows[0]}\n{all_rows[1]}\n{all_rows[2]}\n{all_rows[3]}\n{all_rows[4]}")
+
+
+
+
+def run():
+    retrieve_bot()
+
+
+if __name__ == "__main__":
+    run()
+
--- persistence/select_one.py
+++ persistence/select_one.py
@@ -1,14 +1,14 @@
-import sqlite3
+import sqlite3                                         # Imports SQLite
 
 
 def retrieve_bot():
-    db = sqlite3.connect("bots.db")
+    db = sqlite3.connect("bots.db")                     # Connects to the database 'bots.db'
     cursor = db.cursor()
 
-    sql = "SELECT * FROM bots;"
-    cursor.execute(sql)
+    sql = "SELECT * FROM bots;"                         # This takes the SQLite query to be executed as a parameter (string) value.
+    cursor.execute(sql)                                 # Cursor is used to query the database
 
-    record = cursor.fetchone()
+    record = cursor.fetchone()                          # Method 'fetchone' gets a single record from the db
     db.close()
 
     print(record)
